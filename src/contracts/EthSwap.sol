81cd381f0ce5346922d8a12586d4dd5f49e7b4fe
Ken MacKeen <wellnessgardener@gmail.com>
2021-01-29 10:48:44 -0500

    r2v3

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,23 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol";
+
 contract EthSwap {
 	string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint public rate = 100;
+
+   constructor(Token _token) public {
+   token = _token;		
+   }
+   function buyTokens() public payable{
+    // transfer tokens to buyer msg.sender - msg is global variable and sender is the address calling the function
+   	// redemption rate - the # of tokens they receive for 1 ether
+    // amount of ethereum * redemption rate. value is special variable in solidity that
+    // tells you how much eth was sent hen the function was called.
+   		//calculate the number of tokens to buy
+   		uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+     
+   }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -5,11 +5,23 @@ require('chai')
 	.use(require('chai-as-promised'))
 	.should()
 
-contract('EthSwap', (accounts) => {
+function tokens(n) {
+	return web3.utils.toWei(n, 'ether')
+}
+
+contract('EthSwap', ([deployer, investor]) => {
+	let token, ethSwap
+
+	before(async () => {
+		token = await Token.new()
+		ethSwap = await EthSwap.new(token.address)
+		//Transfer all the tokens to EthSwap ( 1 million)
+		await token.transfer(ethSwap.address, tokens('1000000'))
+
+	})
 
 describe('Token deployment', async() => {
 		it('contract has a name', async() => {
-			let token = await Token.new()
 			const name = await token.name()
 			assert.equal(name, 'DApp Token')
 		})
@@ -17,19 +29,26 @@ describe('Token deployment', async() => {
 
 describe('EthSwap deployment', async() => {
 		it('contract has a name', async() => {
-			let ethSwap = await EthSwap.new()
 			const name = await ethSwap.name()
 			assert.equal(name, 'EthSwap Instant Exchange')
 		})
-
+	
 		it('contract has tokens', async () => {
-			let token = await Token.new()
-			let ethSwap = await EthSwap.new()
-			await token.transfer(ethSwap.address, '1000000000000000000000000')
 			let balance = await token.balanceOf(ethSwap.address)
-			assert.equal(balance.toString(), '1000000000000000000000000')
-
+			assert.equal(balance.toString(), tokens('1000000'))
 		})
-	})
+	})	
 
 })
+
+
+
+
+
+		
+		
+
+		
+	
+
+
